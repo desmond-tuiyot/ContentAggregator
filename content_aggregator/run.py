1499316f519831ba78478f27ca538485a99cce13
Desmond Tuiyot <dtuiyot@gmail.com>
2020-06-04 17:16:45 -0400

    deployment changes more

--- content_aggregator/run.py
+++ content_aggregator/run.py
@@ -1,6 +1,13 @@
-from content_frontend import create_app
+from content_frontend import create_app, db
+from content_frontend.models import Source, Post
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Source': Source, 'Post': Post}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
