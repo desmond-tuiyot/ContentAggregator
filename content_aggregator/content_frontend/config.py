4df28530b45988b4e634f72af481e97da177bf91
Desmond Tuiyot <dtuiyot@gmail.com>
2020-06-05 03:11:23 -0400

    more debugging

--- content_aggregator/content_frontend/config.py
+++ content_aggregator/content_frontend/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')  # configure later
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # configure later
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
